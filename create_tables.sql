3a169c5bb27213316cc783dfb149744c78882aee
SigmaWolf <quynhnhi7bpct@gmail.com>
2025-11-09 00:57:05 +0700

    create COMPANY database

---
+++ create_tables.sql
@@ -0,0 +1,67 @@
+CREATE TABLE EMPLOYEE (
+	Fname	VARCHAR(15)		NOT NULL, 
+	Minit	CHAR, 
+	Lname	VARCHAR(15)		NOT NULL, 
+	Ssn		CHAR(9)			NOT NULL, 
+	Bdate	DATE, 
+	Address	VARCHAR(30), 
+	Sex		CHAR, 
+	Salary	DECIMAL(10, 2),
+	Super_ssn	CHAR(9),
+	Dno		INT				NOT NULL,
+	PRIMARY KEY (Ssn)
+);
+
+GO
+CREATE TABLE DEPARTMENT (
+	Dname	VARCHAR(15)		NOT NULL, 
+	Dnumber	INT				NOT NULL, 
+	Mgr_ssn	CHAR(9)			NOT NULL, 
+	Mgr_start_date	DATE,
+	PRIMARY KEY (Dnumber),
+	UNIQUE (Dname),
+	FOREIGN KEY (Mgr_ssn) REFERENCES EMPLOYEE(Ssn)
+	
+);
+
+GO
+CREATE TABLE DEPT_LOCATIONS (
+	Dnumber		INT				NOT NULL, 
+	Dlocation	VARCHAR(15)		NOT NULL, 
+	PRIMARY KEY (Dnumber, Dlocation),
+	FOREIGN KEY (Dnumber) REFERENCES DEPARTMENT(Dnumber)  
+);
+
+GO
+CREATE TABLE PROJECT (
+	Pname		VARCHAR(15)		NOT NULL, 
+	Pnumber		INT				NOT NULL, 
+	Plocation	VARCHAR(15), 
+	Dnum		INT				NOT NULL, 
+	PRIMARY KEY (Pnumber),
+	UNIQUE (Pname), 
+	FOREIGN KEY (Dnum) REFERENCES DEPARTMENT (Dnumber)
+);
+
+GO
+CREATE TABLE WORKS_ON (
+	Essn			CHAR(9)			NOT NULL,
+	Pno 			INT				NOT NULL,
+	HoursWork		DECIMAL(3, 1)	NOT NULL,
+	PRIMARY KEY (Essn, Pno), 
+	FOREIGN KEY (Essn) REFERENCES EMPLOYEE(Ssn),
+	FOREIGN KEY (Pno) REFERENCES PROJECT(Pnumber) 	
+);
+
+GO
+CREATE TABLE DEPENDENT (
+	Essn			CHAR(9)			NOT NULL, 
+	Dependent_name	VARCHAR(15)		NOT NULL,
+	Sex				CHAR, 
+	Bdate			DATE, 
+	Relationship	VARCHAR(8),
+	PRIMARY KEY (Essn, Dependent_name),
+	FOREIGN KEY (Essn) REFERENCES EMPLOYEE(Ssn)
+);
+
+
