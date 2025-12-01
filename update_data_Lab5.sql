9cbeb2bff530653dbb8f820a41f071f9779a1ebc
SigmaWolf <quynhnhi7bpct@gmail.com>
2025-12-02 00:32:52 +0700

    Bai1-b-update DEPENDENT

--- update_data_Lab5.sql
+++ update_data_Lab5.sql
@@ -3,6 +3,11 @@
 UPDATE  EMPLOYEE
 SET     Address = N'123 Lý Thường Kiệt F.14 Q.10'
 WHERE   Ssn = '123456789'; 
--- b. Mối quan hệ của nhân viên “Franklin” với người phụ thuộc “Joy” thay đổi thành “Friend”. 
+
+-- b. Mối quan hệ của nhân viên “Franklin” với người phụ thuộc “Joy” thay đổi thành “Friend”.
+UPDATE  DEPENDENT
+SET     Relationship = 'Friend'
+WHERE   Dependent_name = 'Joy' AND Essn IN (SELECT Ssn FROM EMPLOYEE WHERE Fname = 'Franklin');
+
 -- c. Tất cả nhân viên của phòng ban có ít nhất một vị trí ở “Houston” được tăng lương gấp đôi. 
 -- d. Trừ 5% lương cho các nhân viên có tổng số dự án tham gia ít hơn 2.
