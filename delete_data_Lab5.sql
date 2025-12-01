37106d31147ac3f2a11b0e0f77e31b7d2038051b
SigmaWolf <quynhnhi7bpct@gmail.com>
2025-12-02 01:25:32 +0700

    Bai1-delete data from DEPENDENT, WORKS_ON and PROJECT

--- delete_data_Lab5.sql
+++ delete_data_Lab5.sql
@@ -1,3 +1,15 @@
 -- Viết câu lệnh để xoá các dữ liệu sau: 
 -- a. Xóa những thân nhân của nhân viên có tên là “Alice". 
+DELETE FROM DEPENDENT
+WHERE   Essn IN (SELECT Ssn
+                FROM    EMPLOYEE
+                WHERE   Fname = 'Alice');
+
 -- b. Xóa dự án “Product Z”. 
+DELETE FROM WORKS_ON
+WHERE   Pno IN (SELECT Pnumber
+                FROM    PROJECT
+                WHERE   Pname = 'Product Z');
+
+DELETE FROM PROJECT
+WHERE   Pname = 'Product Z';
