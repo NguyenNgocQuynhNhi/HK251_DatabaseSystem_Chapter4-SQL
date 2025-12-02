ac27a8cf4010c8d5ac4371ad054730d48ce30380
SigmaWolf <quynhnhi7bpct@gmail.com>
2025-12-02 16:09:45 +0700

    Bai2-a

---
+++ query_data.sql
@@ -0,0 +1,39 @@
+-- a. Hiển thị tên nhân viên, mã phòng ban, lương của nhân viên có lương từ 1000 đến 2000. 
+SELECT  ENAME, DEPTNO, SAL  
+FROM    EMP
+WHERE   SAL  BETWEEN 1000 AND 2000;
+
+-- b. Hiển thị mã phòng ban, tên phòng ban, sắp xếp theo thứ tự tên phòng ban. 
+
+-- c. Hiển thị mã nhân viên, tên nhân viên của phòng ban có mã là 10 hoặc 20. 
+
+-- d. Hiển thị tên nhân viên, nghề nghiệp của nhân viên có nghề là ‘CLERK’, thuộc phòng ban có mã số là 20 
+
+-- e. Hiển thị mã nhân viên, tên nhân viên của nhân viên có tên có chứa chuỗi ‘TH’ hay ‘LL’. 
+
+-- f. Hiển thị tên nhân viên, nghề nghiệp, lương của những nhân viên có giám đốc quản lý. 
+
+-- g. Hiển thị tên nhân viên, mã phòng ban, ngày gia nhập công ty sao cho gia nhập công ty trong năm 1983. 
+
+-- h. Hiển thị tên nhân viên, ngày gia nhập công ty, ngày xét nâng lương (sau ngày gia nhập công ty 1 
+--năm), sắp xếp theo thứ tự ngày xét nâng lương. 
+
+-- i. Tìm lương thấp nhất, lớn nhất và lương trung bình trong tất cả nhân viên. 
+
+-- j.  Tìm lương nhỏ nhất và lớn nhất của mỗi loại nghề nghiệp. 
+
+-- k. Tìm xem có bao nhiêu giám đốc trong danh sách nhân viên. 
+
+-- l. Tìm tất cả các phòng ban mà số nhân viên trong phòng >3. 
+
+-- m. Tìm ra mức lương của mỗi nhân viên làm việc cho một giám đốc nào đó sắp xếp theo thứ tự tăng 
+--dần của mức lương. 
+
+-- n. Hiển thị tên nhân viên, vị trí địa lý, tên phòng với điều kiện lương >1500. 
+
+-- o. Hiển thị tên nhân viên , nghề nghiệp, lương, mức lương, tên phòng làm việc trừ nhân viên có nghề là 
+--cleck và sắp xếp theo chiều giảm của lương. 
+
+-- p. Hiển thị nghề nghiệp được tuyển dụng vào năm 1981 và không được tuyển dụng vào năm 1994. 
+
+-- q. Tìm những nhân viên gia nhập công ty trước giám đốc của họ.
